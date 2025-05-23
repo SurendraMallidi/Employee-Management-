baab07b1d0b4e54e6c004f880ed9afe2681ea9b8
Surendra <surendra318.mallidi@gmail.com>
2025-05-22 22:16:32 -0400

    project added

---
+++ employeemanagement/src/main/java/com/employeemanagement/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.employeemanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer empId;
+    private String empName;
+    private String empAddress;
+    private Long empMobileNum;
+    private String empEmail;
+    private Double empSalary;
+
+}
